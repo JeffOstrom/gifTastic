a9468a16b611f1bbfb61cd2e48a0d2d9005e15aa
Jeff Ostrom <j.ostrm@gmail.com>
2017-10-16 18:17:59 -0500

    updated js, URL

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,27 +1,62 @@
-       
+$(document.body).on("click", ".animal", callApi);       
 
-var topics = ["Chicago Cubs", "Chicago Bears", "Chicago Blackhawks", "Chicago White Sox"];
+var topics = ["tiger", "bear", "parrot", "shark, whale, lion, leopard"];
 
     function renderButton(){
 
-        $("#sports").empty();
+        $("#buttons-view").empty();
 
         for (var i = 0; i < topics.length; i++) {
             
-
-
         var a = $("<button>");
 
-        a.addClass("movie");
+        a.addClass("animal");
 
         a.attr("data-name", topics[i]);
 
         a.text(topics[i]);
 
-        $("sports").append(a);
+        $("#buttons-view").append(a);
 
         }
     }
 
 
-  
+      $("#animals").on("click", function(event) {
+        event.preventDefault();
+        
+        var anima = $("#animal-input").val().trim();
+
+        
+        topics.push(anima);
+
+       
+        renderButton();
+
+        $("#animal-input").val("");
+      });
+
+     
+      renderButton();
+function callApi(){
+      var term = $(this).attr("data-name");
+
+      $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?api_key=rB31IP2gtdNjh8prZrfKi5ELe7gKMcHj",
+        method: "GET",
+        data: {"q": term}
+      }).done(function(response){
+        console.log(this.url)
+
+        var imageURL = response.data.image_original_url;
+
+        var animalImage = $("<img>");
+
+        console.log(response);
+      });
+}
+ 
+
+
+
+  //rB31IP2gtdNjh8prZrfKi5ELe7gKMcHj
