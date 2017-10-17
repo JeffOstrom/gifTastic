6b59cec5908c4a219844d1bb99442c1d59b7f730
Jeff Ostrom <j.ostrm@gmail.com>
2017-10-17 09:42:46 -0500

    js update images

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,8 @@
-$(document.body).on("click", ".animal", callApi);       
 
-var topics = ["tiger", "bear", "parrot", "shark, whale, lion, leopard"];
+$(document.body).on("click", ".animal", callApi); 
+
+
+var topics = ["tiger", "bear", "parrot", "shark", "whale", "lion", "leopard"];
 
     function renderButton(){
 
@@ -8,18 +10,18 @@ var topics = ["tiger", "bear", "parrot", "shark, whale, lion, leopard"];
 
         for (var i = 0; i < topics.length; i++) {
             
-        var a = $("<button>");
+          var a = $("<button>");
 
-        a.addClass("animal");
+          a.addClass("animal");
 
-        a.attr("data-name", topics[i]);
+          a.attr("data-name", topics[i]);
 
-        a.text(topics[i]);
+          a.text(topics[i]);
 
-        $("#buttons-view").append(a);
+          $("#buttons-view").append(a);
 
         }
-    }
+      }
 
 
       $("#animals").on("click", function(event) {
@@ -43,26 +45,45 @@ var topics = ["tiger", "bear", "parrot", "shark, whale, lion, leopard"];
       var term = $(this).attr("data-name");
 
       $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?api_key=rB31IP2gtdNjh8prZrfKi5ELe7gKMcHj",
+        url: "https://api.giphy.com/v1/gifs/search?api_key=rB31IP2gtdNjh8prZrfKi5ELe7gKMcHj&limit=10",
         method: "GET",
         data: {"q": term}
       }).done(function(response){
      
+        $("#view-gif").empty();
 
-        var imageUrl = response.data.image_original_url;
+        for (var i = 0; i < response.data.length ; i++) {
+
+
+        var imageUrl = response.data[i].images.fixed_height_small_still.url;
 
         var animalImage = $("<img>");
 
-        animalImage.attr("src", imgageUrl);
-        animalImage.attr("alt", animalImage);
-
-        console.log(response);
+        animalImage.addClass("gif");
+        animalImage.attr("src", imageUrl);
+        animalImage.attr("alt", "image");
+        animalImage.attr("data-animatedUrl", imageUrl);
+        animalImage.attr("data-stillUrl", imageUrl);
 
         $("#view-gif").prepend(animalImage);
-      });
+
+      }
+
+        console.log(response);
+        
+        //rating
+        console.log("rating:" , response.data[0].rating);
+
+        //animated
+        console.log(response.data[0].images.fixed_height_small.url);
+        //still
+        console.log(response.data[0].images.fixed_height_small_still.url);
+        
+
+    });
 };
  
 
 
 
-  //rB31IP2gtdNjh8prZrfKi5ELe7gKMcHj
+  
