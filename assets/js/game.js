98fdb5eecf2c3f427227a93b4f3989f1764c7dd7
Jeff Ostrom <j.ostrm@gmail.com>
2017-10-17 13:56:16 -0500

    doc ready

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,89 +1,93 @@
-
-$(document.body).on("click", ".animal", callApi); 
-
-
-var topics = ["tiger", "bear", "parrot", "shark", "whale", "lion", "leopard"];
-
-    function renderButton(){
-
-        $("#buttons-view").empty();
-
-        for (var i = 0; i < topics.length; i++) {
-            
-          var a = $("<button>");
-
-          a.addClass("animal");
-
-          a.attr("data-name", topics[i]);
-
-          a.text(topics[i]);
-
-          $("#buttons-view").append(a);
-
-        }
-      }
-
-
-      $("#animals").on("click", function(event) {
-        event.preventDefault();
-        
-        var anima = $("#animal-input").val().trim();
-
-        
-        topics.push(anima);
-
-       
-        renderButton();
-
-        $("#animal-input").val("");
-      });
-
-     
-      renderButton();
+//Ready function always at top
+$(document).ready(function(){
     
-    function callApi(){
-      var term = $(this).attr("data-name");
 
-      $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?api_key=rB31IP2gtdNjh8prZrfKi5ELe7gKMcHj&limit=10",
-        method: "GET",
-        data: {"q": term}
-      }).done(function(response){
-     
-        $("#view-gif").empty();
-
-        for (var i = 0; i < response.data.length ; i++) {
+    $(document.body).on("click", ".animal", callApi); 
 
 
-        var imageUrl = response.data[i].images.fixed_height_small_still.url;
+    var topics = ["tiger", "bear", "parrot", "shark", "whale", "lion", "leopard"];
 
-        var animalImage = $("<img>");
+        function renderButton(){
 
-        animalImage.addClass("gif");
-        animalImage.attr("src", imageUrl);
-        animalImage.attr("alt", "image");
-        animalImage.attr("data-animatedUrl", imageUrl);
-        animalImage.attr("data-stillUrl", imageUrl);
+            $("#buttons-view").empty();
 
-        $("#view-gif").prepend(animalImage);
+            for (var i = 0; i < topics.length; i++) {
+                
+              var a = $("<button>");
 
-      }
+              a.addClass("animal");
 
-        console.log(response);
+              a.attr("data-name", topics[i]);
+
+              a.text(topics[i]);
+
+              $("#buttons-view").append(a);
+
+            }
+          }
+
+
+          $("#animals").on("click", function(event) {
+            event.preventDefault();
+            
+            var anima = $("#animal-input").val().trim();
+
+            
+            topics.push(anima);
+
+           
+            renderButton();
+
+            $("#animal-input").val("");
+          });
+
+         
+          renderButton();
         
-        //rating
-        console.log("rating:" , response.data[0].rating);
+        function callApi(){
+          var term = $(this).attr("data-name");
 
-        //animated
-        console.log(response.data[0].images.fixed_height_small.url);
-        //still
-        console.log(response.data[0].images.fixed_height_small_still.url);
-        
+          $.ajax({
+            url: "https://api.giphy.com/v1/gifs/search?api_key=rB31IP2gtdNjh8prZrfKi5ELe7gKMcHj&limit=10",
+            method: "GET",
+            data: {"q": term}
+          }).done(function(response){
+         
+            $("#view-gif").empty();
 
-    });
-};
- 
+            for (var i = 0; i < response.data.length ; i++) {
 
+              
+
+            var imageUrl = response.data[i].images.fixed_height_small_still.url;
+
+            var animalImage = $("<img>");
+
+            // This adds attributes to the image. I got help with this section from tutor since was confused.
+            animalImage.addClass("gif");
+            animalImage.attr("src", imageUrl);
+            animalImage.attr("alt", "image");
+            animalImage.attr("data-animatedUrl", imageUrl);
+            animalImage.attr("data-stillUrl", imageUrl);
+
+            $("#view-gif").prepend(animalImage);
+
+          }
+
+            console.log(response);
+            
+            //rating
+            console.log("rating:" , response.data[0].rating);
+
+            //animated
+            console.log(response.data[0].images.fixed_height_small.url);
+            //still
+            console.log(response.data[0].images.fixed_height_small_still.url);
+            
+
+        });
+    };
+ });
 
 
   
